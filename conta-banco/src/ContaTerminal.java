300f6384f2865ea8a2696431d6b29b3245163b3f
Asaphe Sabino <asaphhsabino@gmail.com>
2025-03-25 17:17:38 -0300

    projeto feito

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,29 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+
+        Scanner sc = new Scanner(System.in);
+
+        int numero;
+        String agencia;
+        String nomeCliente;
+        double saldo;
+
+        System.out.println("Por favor digite o numero da conta: ");
+        numero = sc.nextInt();
+
+        System.out.println("Agencia: ");
+        agencia = sc.next();
+        sc.nextLine();
+        System.out.println("Nome Cliente: ");
+        nomeCliente = sc.nextLine();
+
+        System.out.println("Saldo: ");
+        saldo = sc.nextDouble();
+
+        System.out.printf("Ola " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agencia e " + agencia + " conta " + numero + " e seu saldo " + saldo + " ja esta disponivel para saque");
+
+        sc.close();
     }
 }
